97633f2b1ced7f405b2bcb7e48fd6cbd0aab2dc3
admin <2750427226@qq.com>
2024-01-17 00:30:27 +0800

    axios基础封装

--- src/main.js
+++ src/main.js
@@ -8,6 +8,13 @@ import router from './router'
 
 const app = createApp(App)
 
+// 测试接口函数
+import {getCategory} from '@/apis/TestApi';
+
+getCategory().then(res=>{
+  console.log(res)
+})
+
 app.use(createPinia())
 app.use(router)
 
