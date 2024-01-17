6656f4c58e69f9c408b02bc2e5846e5f6f58d897
admin <2750427226@qq.com>
2024-01-17 14:26:03 +0800

    吸附导航交互实现

--- src/main.js
+++ src/main.js
@@ -8,12 +8,7 @@ import router from './router'
 
 const app = createApp(App)
 
-// 测试接口函数
-import {getCategory} from '@/apis/TestApi';
 
-getCategory().then(res=>{
-  console.log(res)
-})
 
 app.use(createPinia())
 app.use(router)
