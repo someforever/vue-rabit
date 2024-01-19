8174311ec005c1e798c2fe07f95a7d295fe23f4a
admin <2750427226@qq.com>
2024-01-19 17:44:26 +0800

    详情-整体认识与路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import layout from '@/views/layout/index.vue'
 import home from '@/views/home/index.vue'
 import category from '@/views/category/index.vue'
 import subCategory from '@/views/subCategory/index.vue'
+import Detail from '@/views/Detail/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -23,6 +24,10 @@ const router = createRouter({
         {
           path:'category/sub/:id',
           component:subCategory
+        },
+        {
+          path: 'detail/:id',
+          component: Detail
         }
       ]
     },
