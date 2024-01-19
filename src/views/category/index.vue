ecf05abde2057ef665f1b825fd8c1891be9fef05
admin <2750427226@qq.com>
2024-01-19 14:46:25 +0800

    一级分类-面包屑导航渲染

--- src/views/category/index.vue
+++ src/views/category/index.vue
@@ -1,12 +1,108 @@
-<template>
-  <div>这是category页面</div>  
-    
-</template>
+<script setup>
+import {GetCategoryAPI} from '@/apis/Category'
+import {ref,onMounted} from 'vue';
+import {useRoute} from 'vue-router';
+const CategoryDate = ref({})
+const route = useRoute()
+const GetCategory = async ()=>{
+  const res = await GetCategoryAPI(route.params.id)
+  CategoryDate.value = res.result
+}
 
-<script lang="ts" setup name="">
-  
+onMounted(() =>GetCategory())
 </script>
 
-<style>
-  
+<template>
+  <div class="top-category">
+    <div class="container m-top-20">
+      <!-- 面包屑 -->
+      <div class="bread-container">
+        <el-breadcrumb separator=">">
+          <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+          <el-breadcrumb-item>{{CategoryDate.name}}</el-breadcrumb-item>
+        </el-breadcrumb>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<style scoped lang="scss">
+.top-category {
+  h3 {
+    font-size: 28px;
+    color: #666;
+    font-weight: normal;
+    text-align: center;
+    line-height: 100px;
+  }
+
+  .sub-list {
+    margin-top: 20px;
+    background-color: #fff;
+
+    ul {
+      display: flex;
+      padding: 0 32px;
+      flex-wrap: wrap;
+
+      li {
+        width: 168px;
+        height: 160px;
+
+
+        a {
+          text-align: center;
+          display: block;
+          font-size: 16px;
+
+          img {
+            width: 100px;
+            height: 100px;
+          }
+
+          p {
+            line-height: 40px;
+          }
+
+          &:hover {
+            color: $xtxColor;
+          }
+        }
+      }
+    }
+  }
+
+  .ref-goods {
+    background-color: #fff;
+    margin-top: 20px;
+    position: relative;
+
+    .head {
+      .xtx-more {
+        position: absolute;
+        top: 20px;
+        right: 20px;
+      }
+
+      .tag {
+        text-align: center;
+        color: #999;
+        font-size: 20px;
+        position: relative;
+        top: -20px;
+      }
+    }
+
+    .body {
+      display: flex;
+      justify-content: space-around;
+      padding: 0 40px 30px;
+    }
+  }
+
+  .bread-container {
+    padding: 25px 0;
+  }
+}
 </style>
