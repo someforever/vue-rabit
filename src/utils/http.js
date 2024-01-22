9a6629d6c7a59ec4c288d18616e837c96444518d
admin <2750427226@qq.com>
2024-01-22 20:51:25 +0800

    token失效401拦截处理

--- src/utils/http.js
+++ src/utils/http.js
@@ -4,6 +4,7 @@ import { ElMessage } from 'element-plus'
 import 'element-plus/theme-chalk/el-message.css'
 import {useUserStore} from '@/stores/user'
 
+import router from '@/router'
 const httpInstance = axios.create({
     baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
     timeout: 5000
@@ -22,11 +23,19 @@ const httpInstance = axios.create({
   }, e => Promise.reject(e))
 //axios响应式拦截器
 httpInstance.interceptors.response.use(res => res.data, e => {
-  // 错误提示
+  const userStore = useUserStore()
+  // 统一错误提示
   ElMessage({
     type: 'warning',
     message: e.response.data.message
   })
+  // 401 token失效处理
+  //1.清除本地用户数据
+  //2.跳转登录
+  if(e.response.status===401){
+    userStore.clearUserInfo()
+    router.push('/login')
+  }
   return Promise.reject(e)
 })
 export default httpInstance
