d3544091918e3cc5640e7b115514295dbb6dd3e1
admin <2750427226@qq.com>
2024-01-22 20:10:46 +0800

    请求拦截器携带token

--- src/utils/http.js
+++ src/utils/http.js
@@ -2,6 +2,8 @@
 import axios from 'axios'
 import { ElMessage } from 'element-plus'
 import 'element-plus/theme-chalk/el-message.css'
+import {useUserStore} from '@/stores/user'
+
 const httpInstance = axios.create({
     baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
     timeout: 5000
@@ -9,6 +11,13 @@ const httpInstance = axios.create({
   //  拦截器
   //axios请求拦截器
   httpInstance.interceptors.request.use(config => {
+    // 1.获取pinia里面的token数据
+    const userStore = useUserStore()
+    // 2.通过后端的要求拼接token数据
+    const token = userStore.userInfo.token
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
     return config
   }, e => Promise.reject(e))
 //axios响应式拦截器
