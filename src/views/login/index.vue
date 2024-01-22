8a6b9d84385e0dd6259ef8436e439cd029c9e606
admin <2750427226@qq.com>
2024-01-22 17:32:23 +0800

    表单校验实现

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,5 +1,20 @@
 <script setup>
-
+import {ref} from 'vue'
+// 绑定表单
+const form = ref({
+  account: '',
+  password: ''
+})
+// 准备规则对象
+const rules = {
+  account:[
+    {required: true, message: "用户名不能为空", trigger: 'blur' }
+  ],
+  password:[
+  {required: true, message: "密码不能为空", trigger: 'blur' },
+  {mix:6, max:14, message: "密码长度在6到14之间", trigger: 'blur' }
+  ]
+}
 </script>
 
 
@@ -24,13 +39,13 @@
         </nav>
         <div class="account-box">
           <div class="form">
-            <el-form label-position="right" label-width="60px"
+            <el-form :model="form" :rules="rules" label-position="right" label-width="60px"
               status-icon>
-              <el-form-item  label="账户">
-                <el-input/>
+              <el-form-item prop="account" label="账户">
+                <el-input v-model="form.account" />
               </el-form-item>
-              <el-form-item label="密码">
-                <el-input/>
+              <el-form-item prop="password" label="密码">
+                <el-input v-model="form.password" />
               </el-form-item>
               <el-form-item label-width="22px">
                 <el-checkbox  size="large">
