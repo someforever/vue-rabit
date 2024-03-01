445563150be76f80c29b3eb428643453eec8f5a4
someforever <2750427226@qq.com>
2024-03-01 17:33:37 +0800

    会员中心-细节优化

--- src/router/index.js
+++ src/router/index.js
@@ -57,7 +57,7 @@ const router = createRouter({
           component: Member,
           children:[
             {
-              path: 'user',
+              path: '',
               component: UserInfo
             },
             {
